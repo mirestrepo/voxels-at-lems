fb1a68dfa565c2eb00e335dec2f1bd55f0204899
Isabel Restrepo <isabelrestre@gmail.com>
2012-05-18 19:34:36 -0400

    minor changes

--- boxm2/run_main_batch.sh
+++ boxm2/run_main_batch.sh
@@ -1,17 +1,34 @@
 #!/bin/bash
 
+#./main_busy.sh 1 12 3
+#./main_busy.sh 2 12 4
+#./main_busy.sh 3 19 3
+#./main_busy.sh 4 15 4
+./main_busy.sh 6 14 4
+#./main_busy.sh 7 14 4
+#./main_busy.sh 8 15 4
 #./main_busy.sh 9  10 4
 #./main_busy.sh 10 22 2
 #./main_busy.sh 11 17 3
 #./main_busy.sh 12 21 2
 #./main_busy.sh 13 15 3
-#./main_busy.sh 14 13 3
+#./main_busy.sh 14 13 4
+#./main_busy.sh 16 21 3
+#./main_busy.sh 17 33 3  
+#./main_busy.sh 18 21 2
 
-#for (( i=1; i<32; i++ ))
-for i in 3
-do
-
-./main_busy.sh $i
-
-done
+#./main_busy.sh 15 16 3
+#./main_busy.sh 19 14 3
+#./main_busy.sh 20 23 2
+#./main_busy.sh 21 23 2
+#./main_busy.sh 22 15 3
+#./main_busy.sh 23 17 3
+#./main_busy.sh 24 15 3
+#./main_busy.sh 25 16 3
+#./main_busy.sh 26 24 2
+#./main_busy.sh 27 22 2
+#./main_busy.sh 28 18 3
+#./main_busy.sh 29 18 3
+#./main_busy.sh 30 13 3
+#./main_busy.sh 31 24 2
 
