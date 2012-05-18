4115f9a4a2d66e8907e767db46244c9fc172bf6e
Isabel Restrepo <isabelrestre@gmail.com>
2012-05-18 19:38:35 -0400

    dummy helper file

---
+++ temp.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+#
+#for (( i=17; i<32; i++ ))
+#do
+#
+#    find ./site_$i/imgs -iname "*.png" -type f | wc -l
+#
+#done
+
+#cd /Users/isa/Experiments/BOF/helicopter_providence/beaudet_corners/bof_cross_validation
+#
+#t=90;
+#find ./*/thresh_$t/k_means_20 -iname "run_time.txt" -type f | while read filepath
+##find ./*/thresh_$t/classification_20 -iname "run_time.txt" -type f | while read filepath
+#do
+#
+#   count=`cat $filepath`
+#   echo "$filepath: $count"
+#done
+#
+#echo ""
+#
+#find ./*/thresh_$t/class_histograms_20 -iname "run_time.txt" -type f | while read filepath
+#do
+#
+#   count=`cat $filepath`
+#   echo "$filepath: $count"
+#done
+#
+#echo ""
+#
+#find ./*/thresh_$t/classification_20 -iname "run_time.txt" -type f | while read filepath
+#do
+#
+#   count=`cat $filepath`
+#   echo "$filepath: $count"
+#done
+
+#cd /Users/isa/Experiments/BOF/helicopter_providence/corners/k_3/bof_cross_validation
+#
+#t=90;
+#find ./trial_*/thresh_$t/k_means_20 -iname "run_time.txt" -type f | while read filepath
+##find ./*/thresh_$t/classification_20 -iname "run_time.txt" -type f | while read filepath
+#do
+#
+#   count=`cat $filepath`
+#   echo "$filepath: $count"
+#done
+#
+#echo ""
+#
+#find ./*/thresh_$t/class_histograms_20 -iname "run_time.txt" -type f | while read filepath
+#do
+#
+#   count=`cat $filepath`
+#   echo "$filepath: $count"
+#done
+#
+#echo ""
+#
+#find ./*/thresh_$t/classification_20 -iname "run_time.txt" -type f | while read filepath
+#do
+#
+#   count=`cat $filepath`
+#   echo "$filepath: $count"
+#done
+
+ 
