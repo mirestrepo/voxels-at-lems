fd814bd2c9f422f30279c839bd773d3b2fb512ae
Isabel Restrepo <isabelrestre@gmail.com>
2012-09-26 15:08:17 -0400

    Minor changes to pipeline being run

--- registration_eval/run.sh
+++ registration_eval/run.sh
@@ -1,32 +1,36 @@
 #!/bin/bash
 
-n_iter_ia=100;
-n_iter_icp=100;
+for iter in 20; do
+  
+  n_iter_ia=$iter;
+  n_iter_icp=$iter;
 
-for t in 0 1 2 3 4; do
-  ./reg3d_main.py --trial $t --reg_ia true --descriptor "FPFH" --n_iter $n_iter_ia &
-done
-wait
+  for t in 0 1 2 3 4 5 6 7 8 9; do
+    ./reg3d_main.py --trial $t --reg_ia true --descriptor "FPFH" --n_iter $n_iter_ia &
+  done
+  wait
 
-for t in 0 1 2 3 4; do
-  ./reg3d_main.py --trial $t --reg_icp true --descriptor "FPFH" --n_iter $n_iter_icp &
-done
-wait
-for t in 0 1 2 3 4; do
-  ./reg3d_main.py --trial $t --reg_icp true --descriptor "FPFH" --rej_normals true --n_iter $n_iter_icp &
-done
-wait
+  for t in 0 1 2 3 4 5 6 7 8 9; do
+    ./reg3d_main.py --trial $t --reg_icp true --descriptor "FPFH" --n_iter $n_iter_icp &
+  done
+  wait
+  for t in 0 1 2 3 4 5 6 7 8 9; do
+    ./reg3d_main.py --trial $t --reg_icp true --descriptor "FPFH" --rej_normals true --n_iter $n_iter_icp &
+  done
+  wait
 
-for t in 0 1 2 3 4; do
-  ./reg3d_main.py --trial $t --reg_ia true --descriptor "SHOT" --n_iter $n_iter_ia &
-done
-wait
+  for t in 0 1 2 3 4 5 6 7 8 9; do
+    ./reg3d_main.py --trial $t --reg_ia true --descriptor "SHOT" --n_iter $n_iter_ia &
+  done
+  wait
 
-for t in 0 1 2 3 4; do
-  ./reg3d_main.py --trial $t --reg_icp true --descriptor "SHOT" --n_iter $n_iter_icp &
+  for t in 0 1 2 3 4 5 6 7 8 9; do
+    ./reg3d_main.py --trial $t --reg_icp true --descriptor "SHOT" --n_iter $n_iter_icp &
+  done
+  wait
+  for t in 0 1 2 3 4 5 6 7 8 9; do
+    ./reg3d_main.py --trial $t --reg_icp true --descriptor "SHOT" --rej_normals true --n_iter $n_iter_icp &
+  done
+  wait
+  
 done
-wait
-for t in 0 1 2 3 4; do
-  ./reg3d_main.py --trial $t --reg_icp true --descriptor "SHOT" --rej_normals true --n_iter $n_iter_icp &
-done
-wait
