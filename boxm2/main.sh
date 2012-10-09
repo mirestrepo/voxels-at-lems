f5543a17727f31f02193370078ab4b50bb30feba
Isabel Restrepo <isabelrestre@gmail.com>
2012-10-09 12:38:27 -0400

    Added normal estimation code in boxm2 module

--- boxm2/main.sh
+++ boxm2/main.sh
@@ -33,31 +33,39 @@ render_cropped=false;
 render_interactive=false;
 render_trajectory=false;
 compute_gauss_gradients=false;
+compute_normals=false;
+flip_normals=false;
+export_scene=false;
+thresh_PLY=false;
 use_probe=false;
 
 
-create_scene_from_xml=true;
+# create_scene_from_xml=true;
 #build_model=true;
 #crop_scene=true;
-#render=true; 
+#render=true;
 #render_cropped=true;
 #render_interactive=true;
 #render_circle=true;
 #render_trajectory=true;
 #compute_gauss_gradients=true;
+compute_normals=true;
+# flip_normals=true;
+# export_scene=true;
+# thresh_PLY=true;
 #use_probe=true;
 
 
 
-#******************************************************************************************************* 
+#*******************************************************************************************************
 # DEFINE PATHS
 #*******************************************************************************************************
 #Top directory containing frams_original
 #root_dir="/volumes/vision/video/helicopter_providence/3d_models_3_12/site_1";
-root_dir="/data/helicopter_providence_3_12/site_12";
-
+# root_dir="/data/helicopter_providence_3_12/site_12";
+root_dir="/data/hemenways"
 # directory where boxm2 scene is stored
-model_dirname="model";
+model_dirname="color_model";
 boxm2_dir=$root_dir/$model_dirname;
 
 
@@ -82,7 +90,7 @@ fi
 
 
 #*******************************************************************************************************
-# Convert images to grey 
+# Convert images to grey
 #*******************************************************************************************************
 if $convert_to_grey; then
     python rgb_to_grey.py --rgb_dir $root_dir/frames --grey_dir $root_dir/frames_grey
@@ -105,7 +113,7 @@ fi
 # Build the Scene
 #*******************************************************************************************************
 if $build_model; then
-    
+
     #To build the model we brake the image set in chucks because leaks on the GPU cache can corrupt the scene
     #We are specially conservative while we refine
 
@@ -126,16 +134,16 @@ if $build_model; then
         for((chunk=0; chunk < CHUNKS; chunk++))
         do
            python build_model.py -s $root_dir -x "$model_dirname/$scene_file" --imtype "$imtype" -g $device_name -v .06 --initFrame $chunk --skipFrame $CHUNKS --clearApp 1
-           
+
            status=${?}
-           if [ $status -eq 1 ]; then 
+           if [ $status -eq 1 ]; then
                 echo "status: $status"
                 failed=$(($failed+1))
                 failed_r=$(($failed_r+1))
                 echo "[Error] Something Failed. Refine ON. Iteration $i, Chunck $chunk. Num errors $failed"
                 #exit -1
            fi
-           if [ $status -eq 2 ]; then 
+           if [ $status -eq 2 ]; then
                 failed=$(($failed+1))
                 failed_u=$(($failed_u+1))
                 echo "[Error] Something Failed. Refine ON. Iteration $i, Chunck $chunk. Num errors $failed"
@@ -155,16 +163,16 @@ if $build_model; then
         for((chunk=0; chunk < CHUNKS; chunk++))
         do
             python build_model.py -s $root_dir -x "$model_dirname/$scene_file" --imtype "$imtype" -g $device_name -v .06 --refineoff 1 --initFrame $chunk --skipFrame $CHUNKS
-            
+
             status=${?}
             echo "status: $status"
-            if [ $status -eq 1 ]; then 
+            if [ $status -eq 1 ]; then
                 failed2=$(($failed2+1))
                 failed_r2=$(($failed_r2+1))
                 echo "[Error] Something Failed. Refine OFF. Iteration $i, Chunck $chunk. Num errors $failed2"
                 #exit -1
             fi
-            if [ $status -eq 2 ]; then 
+            if [ $status -eq 2 ]; then
                 failed2=$(($failed2+1))
                 failed_u2=$(($failed_u2+1))
                 echo "[Error] Something Failed. Refine OFF. Iteration $i, Chunck $chunk. Num errors $failed2"
@@ -189,13 +197,66 @@ if $compute_gauss_gradients; then
 fi
 
 #*******************************************************************************************************
+# Compute Normals
+#*******************************************************************************************************
+if $compute_normals; then
+  T=$(date +%s);
+  log_file="$root_dir/compute_normals_log.txt"
+  scene_file="scene.xml"
+  device_name="gpu1";
+  $SCRIPTS_PATH/boxm2/boxm2_compute_normals.py -s $root_dir -x "$model_dirname/$scene_file" -g $device_name
+  DIFF=$(( $(date +%s) - $T ))
+  time_file=$root_dir/"compute_normals_run_time.txt";
+  echo "compute_normals.py took (in seconsd) \n $DIFF" > $time_file;
+fi
+
+if $flip_normals; then
+  scene_file="scene.xml"
+  device_name="gpu1";
+  flipped=0;
+  attempt=0;
+  try=1;
+  while [ $try -eq 1 ];
+  do
+    log_file="$root_dir/flip_normals_log.txt"
+    $SCRIPTS_PATH/boxm2/boxm2_flip_normals.py -s $root_dir -x "$model_dirname/$scene_file" -g $device_name --use_sum true -p $log_file
+    status=${?}
+    echo "Status: $status"
+    if [ $status -eq 0 ]; then
+      flipped=1;
+      echo "Succeeded!"
+    fi
+    attempt=$(($attempt+1));
+    try=$(($((! $flipped)) && $(($attempt<3))));
+  done
+fi
+
+#*******************************************************************************************************
+#export to PLY
+#*******************************************************************************************************
+if $export_scene; then
+    scene_file="scene.xml"
+    device_name="gpu1";
+    $SCRIPTS_PATH/boxm2/boxm2_export_scene_to_PLY.py -s $root_dir -x "$model_dirname/$scene_file" -g $device_name --exportFile "gauss_233_normals.ply" --p_thresh 0.1
+fi
+
+#*******************************************************************************************************
+#Threshold PLY --thresholds are specified within the scrip
+#*******************************************************************************************************
+if $thresh_PLY; then
+$SCRIPTS_PATH/ply_util/thresh_ply.py -s $root_dir -i "$root_dir/gauss_233_normals.ply" -o "$root_dir/gauss_233_normals_pvn"
+fi
+
+
+
+#*******************************************************************************************************
 # Render Viewing Trajectory
 #*******************************************************************************************************
 if $render_circle; then
     scene_file="scene.xml"
     imtype="png"
     device_name="gpu1";
-    python boxm2_render_circle.py -s $root_dir -x "$model_dirname/$scene_file" --imtype "$imtype" -g $device_name 
+    python boxm2_render_circle.py -s $root_dir -x "$model_dirname/$scene_file" --imtype "$imtype" -g $device_name
 fi
 
 #*******************************************************************************************************
@@ -211,14 +272,14 @@ fi
 if $render_cropped; then
     $EXE_PATH/boxm2_ocl_render_view -scene $boxm2_dir/rscene.xml -ni $NI -nj $NJ -gpu_idx 1
 fi
-    
+
 #*******************************************************************************************************
 # Interactive render
 #*******************************************************************************************************
 if $render_interactive; then
     $EXE_PATH/boxm2_ocl_render_view  -scene $boxm2_dir/scene.xml -ni $NI -nj $NJ -camdir $root_dir/cams_krt -imgdir $root_dir/imgs -gpu_idx 1
 fi
-    
+
 
 #*******************************************************************************************************
 # Render spacetime using a trajectory
